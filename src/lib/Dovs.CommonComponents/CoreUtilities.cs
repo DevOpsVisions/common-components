6b9a9a50762d0cfa21b26b3abba89c37347c0fc6
Mohamed Radwan <mohamed_radwan_msf@hotmail.com>
2024-09-12 23:21:32 +0100

    Add the project with CoreUtilities

---
+++ src/lib/Dovs.CommonComponents/CoreUtilities.cs
@@ -0,0 +1,20 @@
+﻿namespace Dovs.CommonComponents
+{
+    public class CoreUtilities
+    {
+        public static string GetWelcomeMessage(string appName)
+        {
+            // ASCII art logo and welcome message
+            string welcomeMessage = @"
+  ____              ___             __     ___     _                 
+ |  _ \  _____   __/ _ \ _ __  ___  \ \   / (_)___(_) ___  _ __  ___ 
+ | | | |/ _ \ \ / / | | | '_ \/ __|  \ \ / /| / __| |/ _ \| '_ \/ __|
+ | |_| |  __/\ V /| |_| | |_) \__ \   \ V / | \__ \ | (_) | | | \__ \
+ |____/ \___| \_/  \___/| .__/|___/    \_/  |_|___/_|\___/|_| |_|___/
+                        |_|                                          
+";
+            return welcomeMessage + "\nWelcome to " + appName + " by DevOps Visions!\n";
+        }
+
+    }
+}
